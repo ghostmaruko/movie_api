d2daf01c37b28ad4f760a81bf62a5f6bda293121
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-08-27 16:15:51 +0200

    feat: add CORS in express

--- index.js
+++ index.js
@@ -18,6 +18,25 @@ mongoose.connect("mongodb://localhost:27017/movie_api", {
   useUnifiedTopology: true,
 });
 
+// =================== CORS ===================
+const cors = require("cors");
+app.use(cors());
+
+let allowedOrigins = [
+  "http://localhost:8080", 'http://mio-frontend.com'];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      let message = `The CORS policy for this application doesn't allow access from origin ${origin}.`;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
+
 // =================== MIDDLEWARE ===================
 app.use(express.json());
 app.use(passport.initialize());
