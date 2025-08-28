a1189d023168d22516bb491facca47ce6ccdce1d
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-08-28 18:13:33 +0200

    add demo users

---
+++ users_demo.js
@@ -0,0 +1,26 @@
+[
+  {
+    Username: "demo1",
+    Password: "demo_pw1",
+    Email: "demo1@email.com",
+    Birthday: "1990-01-01",
+  },
+  {
+    Username: "demo2",
+    Password: "demo_pw2",
+    Email: "demo2@email.com",
+    Birthday: "1992-05-12",
+  },
+  {
+    Username: "demo3",
+    Password: "demo_pw3",
+    Email: "demo3@email.com",
+    Birthday: "1995-09-20",
+  },
+  {
+    Username: "demo4",
+    Password: "demo_pw4",
+    Email: "demo4@email.com",
+    Birthday: "1988-11-30",
+  },
+];
