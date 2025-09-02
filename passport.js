abeecb6adf2b3d6b7fecf738f2439dad0e59da5e
Marco Esu <102921659+ghostmaruko@users.noreply.github.com>
2025-09-02 17:49:08 +0200

    Fix user registration validation: changed all fields to lowercase to match schema

--- passport.js
+++ passport.js
@@ -31,36 +31,22 @@ const JWT_SECRET = "your_jwt_secret";
   )
 ); */
 
-
 passport.use(
   new LocalStrategy(
     {
-      usernameField: "Username",
-      passwordField: "Password",
+      usernameField: "username",
+      passwordField: "password",
     },
-    async (username, password, callback) => {
-      console.log(`${username} ${password}`);
-      await Users.findOne({ Username: username })
-        .then((user) => {
-          if (!user) {
-            console.log("incorrect username");
-            return callback(null, false, {
-              message: "Incorrect username or password.",
-            });
-          }
-          if (!user.validatePassword(password)) {
-            console.log("incorrect password");
-            return callback(null, false, { message: "Incorrect password." });
-          }
-          console.log("finished");
-          return callback(null, user);
-        })
-        .catch((error) => {
-          if (error) {
-            console.log(error);
-            return callback(error);
-          }
-        });
+    (username, password, done) => {
+      User.findOne({ username: username }, (err, user) => {
+        if (err) return done(err);
+        if (!user) return done(null, false, { message: "Incorrect username." });
+
+        if (!user.validatePassword(password))
+          return done(null, false, { message: "Incorrect password." });
+
+        return done(null, user);
+      });
     }
   )
 );
